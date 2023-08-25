88572750cc4e1f88520e63f2d8333ee2c48d1a91
N1xx <bikotegame@gmail.com>
2023-08-25 17:19:22 +0300

    1_3 Арифметические операции

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -7,5 +7,14 @@ public class Calculator {
         int two = 2;
         int onePlusTwo = one + two;
         System.out.println(onePlusTwo);
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        int fiveMinTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinTwo);
+        System.out.println(fourTimeTwo);
     }
 }
