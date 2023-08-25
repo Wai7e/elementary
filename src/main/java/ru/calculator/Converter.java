1097687714bc4615313340edc3381a4769750281
N1xx <bikotegame@gmail.com>
2023-08-25 22:44:02 +0300

    1.9 Конвертер валюты

---
+++ src/main/java/ru/calculator/Converter.java
@@ -0,0 +1,18 @@
+package ru.calculator;
+
+public class Converter {
+    public static float rubToEur(float value) {
+        return value / 100;
+    }
+
+    public static float rubToDlr(float value) {
+        return value / 90;
+    }
+
+    public static void main(String[] args) {
+        int i = 140;
+        float r1 = rubToEur(i);
+        float r2 = rubToDlr(i);
+        System.out.println(i + " rub is " + r1 + " eur and " + r2 + " dlr");
+    }
+}
